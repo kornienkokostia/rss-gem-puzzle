1eb1d80cf780d398e331a539d2d4e41572defa01
kornienkokostia <kornienkokostia@gmail.com>
2022-10-28 18:02:45 +0300

    fix

--- modules/chose-size-menu.js
+++ modules/chose-size-menu.js
@@ -1,4 +1,4 @@
-import { onPauseBtnClick, onResumeBtnClick } from "./game-logic.js"
+import { onPauseBtnClick, onResumeBtnClick, onStartBtnClick } from "./game-logic.js"
 
 const onSizeClick = (el) => {
     const footerMenuSeparators = [...document.querySelectorAll('.size-menu-separator')]
@@ -9,35 +9,41 @@ const onSizeClick = (el) => {
         document.querySelector('.size-menu-current').style.transform = 'translateX(calc(0vh))'
         footerMenuSeparators[0].classList.add('separator-hidden')
         document.querySelector('.size-menu-current').setAttribute('current-value', "3")
+        onStartBtnClick()
     }
     if (el === footerMenuItems[1] || el === footerMenuItemsMobile[1]) {
         document.querySelector('.size-menu-current').style.transform = 'translateX(calc(17.409vh))'
         footerMenuSeparators[0].classList.add('separator-hidden')
         footerMenuSeparators[1].classList.add('separator-hidden')
         document.querySelector('.size-menu-current').setAttribute('current-value', "4")
+        onStartBtnClick()
     }
     if (el === footerMenuItems[2] || el === footerMenuItemsMobile[2]) {
         document.querySelector('.size-menu-current').style.transform = 'translateX(calc(34.818vh))'
         footerMenuSeparators[1].classList.add('separator-hidden')
         footerMenuSeparators[2].classList.add('separator-hidden')
         document.querySelector('.size-menu-current').setAttribute('current-value', "5")
+        onStartBtnClick()
     }
     if (el === footerMenuItems[3] || el === footerMenuItemsMobile[3]) {
         document.querySelector('.size-menu-current').style.transform = 'translateX(calc(52.227vh))'
         footerMenuSeparators[2].classList.add('separator-hidden')
         footerMenuSeparators[3].classList.add('separator-hidden')
         document.querySelector('.size-menu-current').setAttribute('current-value', "6")
+        onStartBtnClick()
     }
     if (el === footerMenuItems[4] || el === footerMenuItemsMobile[4]) {
         document.querySelector('.size-menu-current').style.transform = 'translateX(calc(69.636vh))'
         footerMenuSeparators[3].classList.add('separator-hidden')
         footerMenuSeparators[4].classList.add('separator-hidden')
         document.querySelector('.size-menu-current').setAttribute('current-value', "7")
+        onStartBtnClick()
     }
     if (el === footerMenuItems[5] || el === footerMenuItemsMobile[5]) {
         document.querySelector('.size-menu-current').style.transform = 'translateX(calc(87.085vh))'
         footerMenuSeparators[4].classList.add('separator-hidden')
         document.querySelector('.size-menu-current').setAttribute('current-value', "8")
+        onStartBtnClick()
     }
 }
 
